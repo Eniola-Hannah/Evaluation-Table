3eff810db2158a6367dac952e123ce5dbe3ff2dd
Eniola Hannah <heniola395@gmail.com>
2022-09-28 18:04:51 +0100

    added stuffs in the function

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 const makeMeATable=()=>{
-    
+    for (var i = 1; i <= firstValue.value; i++) {
+        let result = "<tr>"
+        for (var j = 1; j <= secondValue.value; j++) {  
+            parseInt(firstValue.value)
+            parseInt(secondValue.value)
+            if (selection.value == "*") {
+                evaluation = `${i} * ${j} = ${i*j}`
+                result += `
+                    <td>${evaluation}</td>`
+                result += "</tr>"
+                evalTable.innerHTML += result
+
+            }
+            else if (selection.value == "+") {
+                evaluation = `${i} + ${j} = ${i+j}`
+                evalTable.innerHTML += `
+                    <td>${evaluation}</td>`
+            }
+            else if (selection.value == "-") {
+                evaluation = `${i} - ${j} = ${i-j}`
+                evalTable.innerHTML += `
+                    <td> ${evaluation}</td>`
+            }
+            else if (selection.value == "/") {
+                evaluation = `${i} / ${j} = ${i/j}`
+                evalTable.innerHTML += `
+                    <td> ${evaluation}</td>`
+            }
+        } 
+    }
 }
