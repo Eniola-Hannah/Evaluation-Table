2533e29dbbda0b7a9603e3e67acfce4d80fabe0b
Eniola Hannah <heniola395@gmail.com>
2022-09-28 19:06:31 +0100

    added a media folder

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 const makeMeATable=()=>{
-    evalTable.innerHTML = ""
-    for (var i = 1; i <= firstValue.value; i++) { 
-        let result = "<tr>"
-        for (var j = 1; j <= secondValue.value; j++) {  
-            evaluation = `${i} * ${j} = ${i*j}`
-            result += `
-                <td>${evaluation}</td>`
-            result += "</tr>"
-            evalTable.innerHTML += result
-            if (selection.value == "*") {
-
-            }
-            else if (selection.value == "+") {
-                evaluation = `${i} + ${j} = ${i+j}`
+    if (firstValue.value==""&&secondValue=="") {
+        alert("Input a value to be evaluated")
+    }else{
+        evalTable.innerHTML = ""
+        for (var i = 1; i <= firstValue.value; i++) { 
+            let result = "<tr>"
+            for (var j = 1; j <= secondValue.value; j++) {  
+                evaluation = `${i} * ${j} = ${i*j}`
                 result += `
                     <td>${evaluation}</td>`
                 result += "</tr>"
                 evalTable.innerHTML += result
-
-            }
-            else if (selection.value == "-") {
-                evaluation = `${i} - ${j} = ${i-j}`
-                evalTable.innerHTML += `
-                    <td> ${evaluation}</td>`
-            }
-            else if (selection.value == "/") {
-                evaluation = `${i} / ${j} = ${i/j}`
-                evalTable.innerHTML += `
-                    <td> ${evaluation}</td>`
-            }
-        } 
+                if (selection.value == "*") {
+    
+                }
+                else if (selection.value == "+") {
+                    evaluation = `${i} + ${j} = ${i+j}`
+                    result += `
+                        <td>${evaluation}</td>`
+                    result += "</tr>"
+                    evalTable.innerHTML += result
+    
+                }
+                else if (selection.value == "-") {
+                    evaluation = `${i} - ${j} = ${i-j}`
+                    evalTable.innerHTML += `
+                        <td> ${evaluation}</td>`
+                }
+                else if (selection.value == "/") {
+                    evaluation = `${i} / ${j} = ${i/j}`
+                    evalTable.innerHTML += `
+                        <td> ${evaluation}</td>`
+                }
+            } 
+        }
     }
 }
