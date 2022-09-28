4b82f3d7171815f73df7f2c46e07a417ffb5acde
Eniola Hannah <heniola395@gmail.com>
2022-09-28 18:35:09 +0100

    made the interface look nicer

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const makeMeATable=()=>{
-    for (var i = 1; i <= firstValue.value; i++) {
+    evalTable.innerHTML = ""
+    for (var i = 1; i <= firstValue.value; i++) { 
         let result = "<tr>"
         for (var j = 1; j <= secondValue.value; j++) {  
-            parseInt(firstValue.value)
-            parseInt(secondValue.value)
+            evaluation = `${i} * ${j} = ${i*j}`
+            result += `
+                <td>${evaluation}</td>`
+            result += "</tr>"
+            evalTable.innerHTML += result
             if (selection.value == "*") {
-                evaluation = `${i} * ${j} = ${i*j}`
+
+            }
+            else if (selection.value == "+") {
+                evaluation = `${i} + ${j} = ${i+j}`
                 result += `
                     <td>${evaluation}</td>`
                 result += "</tr>"
                 evalTable.innerHTML += result
 
-            }
-            else if (selection.value == "+") {
-                evaluation = `${i} + ${j} = ${i+j}`
-                evalTable.innerHTML += `
-                    <td>${evaluation}</td>`
             }
             else if (selection.value == "-") {
                 evaluation = `${i} - ${j} = ${i-j}`
